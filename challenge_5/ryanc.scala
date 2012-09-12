018a12ad36c61fa5ab1c48a5137eb3f2a4922465
Ryan Croote <ryanc@teamddm.com>
2012-09-12 14:58:23 -0400

    ryanc challenge #5 submission

---
+++ challenge_5/ryanc.scala
@@ -0,0 +1,32 @@
+import scala.collection.mutable.Queue
+import scala.collection.mutable.MutableList
+import scala.collection.mutable.Set
+
+object ryanc {
+	def main(args: Array[String]) = {
+		if (args.length == 0) {
+			println("Please enter some arguments.")
+			System.exit(1)
+		}
+		val argsList = args.toList
+		val start = Integer.parseInt(argsList(0))
+		val end = Integer.parseInt(argsList(1))
+		val queue = Queue[(Int, Set[Int], MutableList[Int])]()
+		queue += ((start, Set[Int](start), MutableList[Int](start)))
+		while (!queue.isEmpty) {
+			val (number, parents, tree) = queue.dequeue
+			val newNumbers = MutableList[Int]( (number / 2), (number * 2), (number + 2) )
+			newNumbers foreach { newNumber =>
+				if (!parents.contains(newNumber)) {
+					val res = tree ++  MutableList[Int](newNumber)
+					parents += newNumber
+					if (newNumber == end) {
+						println(res mkString ",")
+						System.exit(1)
+					}
+					queue += ((newNumber, parents, res))
+				}
+			}
+		}
+	}
+}
